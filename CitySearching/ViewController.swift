544bcb41c8411492f9af9b04c230a3eb2eaea7a0
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2019-06-03 11:46:16 +0700

    first commit

--- CitySearching/ViewController.swift
+++ CitySearching/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["Oklahoma", "Chicago", "Moscow", "Danang", "Vancouver", "Praga", "New York", "London", "Oslo", "Warsaw", "Berlin"] // Mocked API data source
+    let disposeBag = DisposeBag() // Bag of disposables to release them when view is being deallocated
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        searchBar
+            .rx.text // Observable property thanks to RxCocoa
+            .orEmpty // Make it non-optional
+            .subscribe(onNext: { [unowned self] query in // Here we will be notified of every new value
+                self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities.
+                self.tableView.reloadData() // And reload table view data.
+            })
+            .disposed(by: disposeBag)
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
+        
+        return cell
+    }
 
 }
 
